0ae42bfee68ef0d80c94142d9d7066b10bf5f92f
TruongNT <thienthanhoc@gmail.com>
2019-11-12 13:15:21 +0700

    +data manager

---
+++ Assets/Game/Scripts/Data/Const.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const : MonoBehaviour
+{
+    // KEYS
+    public static string IS_LOGIN = "IsLogin";
+    public static string USER_DATA = "User";
+}
